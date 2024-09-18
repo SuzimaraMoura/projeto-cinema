43acb623ca98735629be2e9b911549b25f102224
SuzimaraMoura <suzi.moura.ramalho@gmail.com>
2024-09-17 22:50:15 -0300

    Alterações no import do main

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import React from "react"
 import ReactDOM from "react-dom/client"
-import { BrowserRouter } from "react-router-dom"
+// import { BrowserRouter } from "react-router-dom"
+import { HashRouter as Router } from 'react-router-dom';
 import Router from "./routes/routes"
 import GlobalStyle from "./styles/globalStyle"
 
