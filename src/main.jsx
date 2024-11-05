3b8f981e69bd6f3392cc9d422ae12a0b476088a6
SuzimaraMoura <suzi.moura.ramalho@gmail.com>
2024-11-04 21:00:56 -0300

    Adicionando imagem ao README

--- src/main.jsx
+++ src/main.jsx
@@ -2,14 +2,16 @@ import React from "react"
 import ReactDOM from "react-dom/client"
 // import { BrowserRouter } from "react-router-dom"
 import { HashRouter as Router } from 'react-router-dom';
-import Router from "./routes/routes"
+import AppRouter from "./routes/routes" // Renomeado para AppRouter
 import GlobalStyle from "./styles/globalStyle"
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <Router />
-    </BrowserRouter>
+    <Router>
+      <AppRouter />
+    </Router>
     <GlobalStyle />
   </React.StrictMode>
 );
+
+
